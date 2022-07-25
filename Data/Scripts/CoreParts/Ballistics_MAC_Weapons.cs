f01236d2877ee90e9851ab0811d77f5e8011e8c2
Mike Dude <62247634+Blaylock1988@users.noreply.github.com>
2022-07-24 21:58:21 -0600

    Hopefully fix MAC damage being unreliable

--- Data/Scripts/CoreParts/Ballistics_MAC_Weapons.cs
+++ Data/Scripts/CoreParts/Ballistics_MAC_Weapons.cs
@@ -274,7 +274,7 @@ namespace Scripts
                     BarrelsPerShot = 1, // How many muzzles will fire a projectile per fire event.
                     TrajectilesPerBarrel = 1, // Number of projectiles per muzzle per fire event.
                     SkipBarrels = 0, // Number of muzzles to skip after each fire event.
-                    ReloadTime = 3600, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    ReloadTime = 240, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     MagsToLoad = 1, // Number of physical magazines to consume on reload.
                     DelayUntilFire = 120, // How long the weapon waits before shooting after being told to fire. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     HeatPerShot = 0, // Heat generated per shot.
@@ -332,7 +332,8 @@ namespace Scripts
                 },
             },
             Ammos = new[] {
-                LargeRailgunSabot, // Must list all primary, shrapnel, and pattern ammos.
+                LargeRailgunSabot,
+				MAC_fragment,				// Must list all primary, shrapnel, and pattern ammos.
             },
             Animations = LargeRailgunAnimation,
             //Upgrades = UpgradeModules,
