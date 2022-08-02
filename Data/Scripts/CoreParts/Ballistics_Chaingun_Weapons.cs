bce2985389190c20d457b9c876d46e86051d24d5
Mike Dude <62247634+Blaylock1988@users.noreply.github.com>
2022-08-01 18:26:08 -0600

    Added dual 30mm

--- Data/Scripts/CoreParts/Ballistics_Chaingun_Weapons.cs
+++ Data/Scripts/CoreParts/Ballistics_Chaingun_Weapons.cs
@@ -470,6 +470,125 @@ namespace Scripts {
             //Upgrades = UpgradeModules,
         };
 
+        WeaponDefinition ThrasherTurret => new WeaponDefinition
+        {
+
+            Assignments = new ModelAssignmentsDef
+            {
+                MountPoints = new[] {
+                    new MountPointDef {
+                        SubtypeId = "ARYXHeavyFlakTurret",
+                        SpinPartId = "None",
+                        MuzzlePartId = "MissileTurretBarrels",
+                        AzimuthPartId = "MissileTurretBase1",
+                        ElevationPartId = "MissileTurretBarrels",
+                        DurabilityMod = 0.2f,
+                    },
+
+                },
+                Muzzles = new[] {
+                    "muzzle_projectile_1",
+                    "muzzle_projectile_2",
+                },
+                Ejector = "",
+            },
+            Targeting = new TargetingDef
+            {
+				Threats = new[] {
+					Grids,   // threats percieved automatically without changing menu settings
+				},
+                SubSystems = new[] {
+                    Thrust, Utility, Offense, Power, Production, Jumping, Steering, Any,
+                },
+				ClosestFirst = true, // tries to pick closest targets first (blocks on grids, projectiles, etc...).
+				IgnoreDumbProjectiles = true, // Don't fire at non-smart projectiles.
+				LockedSmartOnly = false, // Only fire at smart projectiles that are locked on to parent grid.
+				MinimumDiameter = 0, // 0 = unlimited, Minimum radius of threat to engage.
+				MaximumDiameter = 0, // 0 = unlimited, Maximum radius of threat to engage.
+				MaxTargetDistance = 1800, // 0 = unlimited, Maximum target distance that targets will be automatically shot at.
+				MinTargetDistance = 0, // 0 = unlimited, Min target distance that targets will be automatically shot at.
+				TopTargets = 0, // 0 = unlimited, max number of top targets to randomize between.
+				TopBlocks = 0, // 0 = unlimited, max number of blocks to randomize between
+				StopTrackingSpeed = 1000, // do not track target threats traveling faster than this speed
+            },
+            HardPoint = new HardPointDef
+            {
+                PartName = "Thrasher Heavy Autocannon", // name of weapon in terminal
+                DeviateShotAngle = 0.15f, // Projectile inaccuracy in degrees.
+                AimingTolerance = 2f, // How many degrees off target a turret can fire at. 0 - 180 firing angle.
+                AimLeadingPrediction = Advanced, // Level of turret aim prediction; Off, Basic, Accurate, Advanced
+                DelayCeaseFire = 0, // Measured in game ticks (6 = 100ms, 60 = 1 second, etc..). Length of time the weapon continues firing after trigger is released.
+                AddToleranceToTracking = false, // Allows turret to track to the edge of the AimingTolerance cone instead of dead centre.
+                CanShootSubmerged = false, // Whether the weapon can be fired underwater when using WaterMod.
+
+                Ui = Common_Weapons_Hardpoint_Ui_FullDisable,
+				
+                Ai = Common_Weapons_Hardpoint_Ai_BasicTurret,
+
+                HardWare = new HardwareDef
+                {
+                    RotateRate = 0.02f,
+                    ElevateRate = 0.015f,
+                    MinAzimuth = -180,
+                    MaxAzimuth = 180,
+                    MinElevation = -5,
+                    MaxElevation = 80,
+                    FixedOffset = false,
+                    InventorySize = 0.480f,
+                    Offset = Vector(x: 0, y: 0, z: 0),
+                    Type = BlockWeapon, // BlockWeapon, HandWeapon, Phantom 
+                    CriticalReaction = new CriticalDef
+                    {
+                        Enable = false, // Enables Warhead behaviour
+                        DefaultArmedTimer = 120,
+                        PreArmed = true,
+                        TerminalControls = true,
+                    },
+                },
+                Other = new OtherDef
+                {
+                    ConstructPartCap = 21,
+                    RotateBarrelAxis = 0,
+                    EnergyPriority = 0,
+                    MuzzleCheck = false,
+                    Debug = false,
+                    RestrictionRadius = 0, // Meters, radius of sphere disable this gun if another is present
+                    CheckInflatedBox = false, // if true, the bounding box of the gun is expanded by the RestrictionRadius
+                    CheckForAnyWeapon = false, // if true, the check will fail if ANY gun is present, false only looks for this subtype
+                },
+                Loading = new LoadingDef
+                {
+                    RateOfFire = 600,
+                    BarrelSpinRate = 0, // visual only, 0 disables and uses RateOfFire
+                    BarrelsPerShot = 1,
+                    TrajectilesPerBarrel = 1, // Number of Trajectiles per barrel per fire event.
+                    SkipBarrels = 0,
+                    ReloadTime = 90, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    DelayUntilFire = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    HeatPerShot = 0, //heat generated per shot
+                    MaxHeat = 0, //max heat before weapon enters cooldown (70% of max heat)
+                    Cooldown = 0, //percent of max heat to be under to start firing again after overheat accepts .2-.95
+                    HeatSinkRate = 0, //amount of heat lost per second
+                    DegradeRof = false, // progressively lower rate of fire after 80% heat threshold (80% of max heat)
+                    ShotsInBurst = 0,
+                    DelayAfterBurst = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    FireFull = false,
+                    GiveUpAfter = false,
+                    MagsToLoad = 2,
+                },
+
+                Audio = Ballistics_Chaingun_Hardpoint_Audio,
+
+                Graphics = Ballistics_Chaingun_Hardpoint_Graphics,
+
+            },
+            Ammos = new[] {
+                AutocannonClip, // Must list all primary, shrapnel, and pattern ammos.
+				AutocannonClip_Ricochet,
+            },
+            //Animations = AryxRiptideAnims,
+            // Don't edit below this line
+        };
 
         // Don't edit below this line.
     }
