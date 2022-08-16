0615bec06a8836d2fde6318dd2cdfc97c7fdac0e
Mike Dude <62247634+Blaylock1988@users.noreply.github.com>
2022-08-15 23:30:12 -0600

    Apparently MaxAbsorb is broken, removed

--- Data/Scripts/CoreParts/Missiles_Torpedo_Ammos.cs
+++ Data/Scripts/CoreParts/Missiles_Torpedo_Ammos.cs
@@ -147,7 +147,7 @@ namespace Scripts
                     Radius = 25f, // Radius of AOE effect, in meters.
                     Damage = 1500000f, //150000f
                     Depth = 25f, // Max depth of AOE effect, in meters. 0=disabled, and AOE effect will reach to a depth of the radius value
-                    MaxAbsorb = 50000f, // Soft cutoff for damage, except for pooled falloff.  If pooled falloff, limits max damage per block.
+                    MaxAbsorb = 0f, // Soft cutoff for damage, except for pooled falloff.  If pooled falloff, limits max damage per block.
                     Falloff = Pooled, //.NoFalloff applies the same damage to all blocks in radius
                     //.Linear drops evenly by distance from center out to max radius
                     //.Curve drops off damage sharply as it approaches the max radius
