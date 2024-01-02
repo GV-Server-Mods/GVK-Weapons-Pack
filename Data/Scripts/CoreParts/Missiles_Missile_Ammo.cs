bd63ab05d5f3994b11c7345c9d5b4a3100c48f1e
Mike Dude <62247634+Blaylock1988@users.noreply.github.com>
2024-01-02 00:25:51 -0700

    reduced archer track delay

--- Data/Scripts/CoreParts/Missiles_Missile_Ammo.cs
+++ Data/Scripts/CoreParts/Missiles_Missile_Ammo.cs
@@ -124,7 +124,7 @@ namespace Scripts
                     Aggressiveness = 3f, // controls how responsive tracking is, recommended value 3-5.
                     MaxLateralThrust = 0.5, // controls how sharp the projectile may turn, this is the cheaper but less realistic version of SteeringLimit, cost of 2 on a scale of 1-5, 0 being basic smart.
                     NavAcceleration = 0, // helps influence how the projectile steers, 0 defaults to 1/2 Aggressiveness value or 0 if its 0, a value less than 0 disables this feature. 
-                    TrackingDelay = 30, // Measured in Shape diameter units traveled.
+                    TrackingDelay = 15, // Measured in Shape diameter units traveled.
                     AccelClearance = false, // Setting this to true will prevent smart acceleration until it is clear of the grid and tracking delay has been met (free fall).
                     MaxChaseTime = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     OverideTarget = false, // when set to true ammo picks its own target, does not use hardpoint's.
