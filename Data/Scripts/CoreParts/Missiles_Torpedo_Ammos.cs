a8ecadedbd51c78619839c0b40cc21d81982232d
Mike Dude <mblaylock1988@gmail.com>
2026-02-04 22:55:43 -0700

    added arming time to reduce point blank use

--- Data/Scripts/CoreParts/Missiles_Torpedo_Ammos.cs
+++ Data/Scripts/CoreParts/Missiles_Torpedo_Ammos.cs
@@ -126,7 +126,7 @@ namespace Scripts
                     //.Pooled damage behaves in a pooled manner that once exhausted damage ceases.
                     //.Exponential drops off exponentially.  Does not scale to max radius
                     ArmOnlyOnHit = false, // Detonation only is available, After it hits something, when this is true. IE, if shot down, it won't explode.
-                    MinArmingTime = 0, // In ticks, before the Ammo is allowed to explode, detonate or similar; This affects shrapnel spawning.
+                    MinArmingTime = 120, // In ticks, before the Ammo is allowed to explode, detonate or similar; This affects shrapnel spawning.
                     NoVisuals = false,
                     NoSound = false,
                     ParticleScale = 1,
